df08b36197f7544a3fd1e94d5c8e6ae6a53f76fd
robertokbr <robertojuniordev@gmail.com>
2022-03-26 17:13:05 -0300

    feat: register prisma client into ads module

--- src/modules/ads/ads.module.ts
+++ src/modules/ads/ads.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
 import { AdsController } from './controllers/ads.controller';
 import { AdsRepository } from './repository/ads.repository';
 import { AdsService } from './services/ads.service';
 
 @Module({
   controllers: [AdsController],
-  providers: [AdsService, AdsRepository],
+  providers: [AdsService, AdsRepository, PrismaClient],
 })
 export class AdsModule {}
