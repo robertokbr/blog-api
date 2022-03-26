e5c14996c4da2d9720b6731a64e69cdb68a92e09
robertokbr <robertojuniordev@gmail.com>
2022-03-26 16:34:59 -0300

    refactor: add api configs and use into initialization

---
+++ src/configs/api.ts
@@ -0,0 +1,3 @@
+export const api = {
+  port: process.env.PORT || 3333,
+};
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 import { swaggerConfig } from './configs/swagger';
+import { api } from './configs/api';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
 
   //pipes
   app.useGlobalPipes(
@@ -22,6 +24,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
 
-  await app.listen(3000);
+  await app.listen(api.port);
 }
 bootstrap();
