2aecd327e0584687ba4c943144c8b7cdc3e7d54c
robertokbr <robertojuniordev@gmail.com>
2022-04-23 21:56:52 -0300

    refactor: reflect changes on main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './modules/common/providers/prisma/prisma.service';
 import { UsersModule } from './modules/users/users.module';
 import { PostsModule } from './modules/posts/posts.module';
 import { AdsModule } from './modules/ads/ads.module';
+import { PrismaModule } from './infra/prisma/prisma.module';
 
 @Module({
-  imports: [UsersModule, PostsModule, AdsModule],
+  imports: [UsersModule, PostsModule, AdsModule, PrismaModule],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -2,28 +2,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { swaggerConfig } from './configs/swagger';
-import { api } from './configs/api';
+import { swaggerConfig } from './infra/configs/swagger.config';
+import { validationPripeConfig } from './infra/configs/validation-pipe.config';
+import { apiConfig } from './infra/configs/api.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-
-  //pipes
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      // whitelist: true,
-      transformOptions: {
-        enableImplicitConversion: true,
-      },
-    }),
-  );
-
+  app.useGlobalPipes(new ValidationPipe(validationPripeConfig));
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
-
-  await app.listen(api.port);
+  await app.listen(apiConfig.port);
 }
+
 bootstrap();
