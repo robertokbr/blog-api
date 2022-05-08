c731ec9b7fc19b4a6e438ae51b7b029588d612fc
robertokbr <robertojuniordev@gmail.com>
2022-05-08 01:46:20 -0300

    feat: improving server monitoring

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,29 @@ import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
 import { PostsModule } from './modules/posts/posts.module';
 import { AdsModule } from './modules/ads/ads.module';
-import { PrismaModule } from './infra/prisma/prisma.module';
+import { PrismaModule } from './infrastructure/prisma/prisma.module';
+import { LoggerModule } from 'nestjs-pino';
 
 @Module({
-  imports: [UsersModule, PostsModule, AdsModule, PrismaModule],
+  imports: [
+    UsersModule,
+    PostsModule,
+    AdsModule,
+    PrismaModule,
+    LoggerModule.forRoot({
+      pinoHttp: {
+        autoLogging: false,
+        quietReqLogger: true,
+        transport: {
+          target: 'pino-http-print', // use the pino-http-print transport and its formatting output
+          options: {
+            all: true,
+            translateTime: true,
+          },
+        },
+      },
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,25 @@
-import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
+import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import { swaggerConfig } from './infra/configs/swagger.config';
-import { validationPripeConfig } from './infra/configs/validation-pipe.config';
-import { apiConfig } from './infra/configs/api.config';
-import 'dotenv/config';
+import { validationPripeConfig } from './infrastructure/configs/validation-pipe.config';
+import { swaggerConfig } from './infrastructure/configs/swagger.config';
+import { apiConfig } from './infrastructure/configs/api.config';
+import { LoggingInterceptor } from './infrastructure/interceptors/logger.interceptor';
+import { Logger as PinoLogger } from 'nestjs-pino';
+import { AllExceptionFilter } from './infrastructure/filters/exceptions-logger.filter';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    bufferLogs: true,
+  });
+
+  app.useLogger(app.get(PinoLogger));
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe(validationPripeConfig));
+  app.useGlobalInterceptors(new LoggingInterceptor(app.get(PinoLogger)));
+  app.useGlobalFilters(new AllExceptionFilter(app.get(PinoLogger)));
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
   await app.listen(apiConfig.port);
