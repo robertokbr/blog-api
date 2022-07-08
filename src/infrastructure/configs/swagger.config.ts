0100523458fcbdac484950b10904b86b3d476c8b
robertokbr <robertojuniordev@gmail.com>
2022-07-08 14:06:11 -0300

    doc: update swagger description

--- src/infrastructure/configs/swagger.config.ts
+++ src/infrastructure/configs/swagger.config.ts
@@ -1,10 +1,10 @@
 import { DocumentBuilder } from '@nestjs/swagger';
 
 export const swaggerConfig = new DocumentBuilder()
-  .setTitle('Social-dev API documentation')
+  .setTitle('blog.rbjr.dev Rest Api documentation.')
   .setVersion('1.0')
   .addBearerAuth()
   .setDescription(
-    'Social-dev Rest Api documentation used by the social-dev web Next app.',
+    'This is the documentation of the API consumed by my personal blog. You can find the repository of the project heading to https://github.com/robertokbr/blog-api.',
   )
   .build();
