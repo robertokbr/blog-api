7704bed3354c8723aea2f957060cccd9d2d53bad
robertokbr <robertojuniordev@gmail.com>
2022-07-30 14:33:45 -0300

    build: update db schema

--- prisma/migrations/20220322152005_/migration.sql
+++
@@ -1,5 +0,0 @@
--- CreateEnum
-CREATE TYPE "PostJoinerStates" AS ENUM ('ACCEPTED', 'DENIED', 'WAITING');
-
--- AlterTable
-ALTER TABLE "PostJoiners" ADD COLUMN     "state" "PostJoinerStates" NOT NULL DEFAULT E'WAITING';
