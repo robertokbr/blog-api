5ea1b349c499068155f989a033e6b0eaea24c5dd
robertokbr <robertojuniordev@gmail.com>
2022-07-30 14:34:06 -0300

    feat: add sentry setup

--- src/main.ts
+++ src/main.ts
@@ -9,19 +9,41 @@ import { apiConfig } from './infrastructure/configs/api.config';
 import { LoggingInterceptor } from './infrastructure/common/interceptors/logger.interceptor';
 import { Logger as PinoLogger } from 'nestjs-pino';
 import { AllExceptionFilter } from './infrastructure/common/filters/exceptions-logger.filter';
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bufferLogs: true,
   });
 
-  app.useLogger(app.get(PinoLogger));
+  Sentry.init({
+    dsn: "https://ba1e80f87e8e40aea5d821028b6d0f0d@o1340494.ingest.sentry.io/6613294",
+    integrations: [
+      // enable HTTP calls tracing
+      new Sentry.Integrations.Http({ tracing: true }),
+      // enable Express.js middleware tracing
+      new Tracing.Integrations.Express({ app: app as any }),
+    ],
+  
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 1.0,
+  });
+
+  app.use(Sentry.Handlers.requestHandler());
+  // TracingHandler creates a trace for every incoming request
+  app.use(Sentry.Handlers.tracingHandler());
+
   app.enableCors();
+  app.useLogger(app.get(PinoLogger));
   app.useGlobalPipes(new ValidationPipe(validationPripeConfig));
   app.useGlobalInterceptors(new LoggingInterceptor(app.get(PinoLogger)));
   app.useGlobalFilters(
     new AllExceptionFilter(app.get(PinoLogger), app.get(HttpAdapterHost)),
   );
+
   const document = SwaggerModule.createDocument(app, swaggerConfig);
   SwaggerModule.setup('docs', app, document);
   await app.listen(apiConfig.port);
