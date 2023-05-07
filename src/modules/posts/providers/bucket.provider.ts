70eff896f2f451536e6ce7257b94ec62da2da486
robertokbr <robertojuniordev@gmail.com>
2023-05-07 14:38:57 -0300

    feat: add bucket provider and image generator provider

--- src/modules/posts/providers/bucket.provider.ts
+++ src/modules/posts/providers/bucket.provider.ts
@@ -1,6 +1,43 @@
+import * as aws from '@aws-sdk/client-s3';
+import { Injectable } from '@nestjs/common';
+import { promises } from 'fs';
+import { resolve } from 'path';
+import { pathsConfig } from 'src/configs/paths.config';
+
+@Injectable()
 export class S3BucketProvider {
-  public async uploadFile(filePath: string) {
-    // upload to aws s2 with aws sdk
-    
+  private bucket: aws.S3;
+
+  constructor() {
+    if (
+      process.env.AWS_ACCESS_KEY_ID === undefined ||
+      process.env.AWS_SECRET_ACCESS_KEY === undefined
+    ) {
+      throw new Error('AWS credentials are not defined');
+    }
+
+    this.bucket = new aws.S3({
+      region: 'us-east-1',
+    });
+  }
+
+  public async uploadFile(filename: string): Promise<string> {
+    const fileContent = await promises.readFile(
+      resolve(pathsConfig.tmp, filename),
+    );
+
+    const params = {
+      Bucket: process.env.AWS_BUCKET_NAME,
+      Key: filename,
+      Body: fileContent,
+    };
+
+    await this.bucket.send(new aws.PutObjectCommand(params));
+
+    const url = `https://${process.env.AWS_BUCKET_NAME}.s3.amazonaws.com/${filename}`;
+
+    await promises.unlink(resolve(pathsConfig.tmp, filename));
+
+    return url;
   }
 }
---
+++ src/modules/posts/providers/image-generator.provider.ts
@@ -0,0 +1,83 @@
+import { Injectable } from '@nestjs/common';
+import fetch from 'node-fetch';
+import { promises } from 'node:fs';
+import { resolve } from 'node:path';
+import { pathsConfig } from 'src/configs/paths.config';
+
+enum StabilityPresets {
+  AnalogFilm = 'analog-film',
+  Anime = 'anime',
+  Cinematic = 'cinematic',
+  ComicBook = 'comic-book',
+  DigitalArt = 'digital-art',
+  Enhance = 'enhance',
+  FantasyArt = 'fantasy-art',
+  Isometric = 'isometric',
+  LineArt = 'line-art',
+  LowPoly = 'low-poly',
+  ModelingCompound = 'modeling-compound',
+  NeonPunk = 'neon-punk',
+  Origami = 'origami',
+  Photographic = 'photographic',
+  PixelArt = 'pixel-art',
+  _3DModel = '3d-model',
+  TileTexture = 'tile-texture',
+}
+
+@Injectable()
+export class StabilityAIImageGeneratorProvider {
+  public async generate(prompt: string, slug: string) {
+    const engineId = 'stable-diffusion-xl-beta-v2-2-2';
+    const apiHost = 'https://api.stability.ai';
+    const apiKey = process.env.STABILITY_API_KEY;
+
+    if (!apiKey) throw new Error('Missing Stability API key.');
+
+    const response = await fetch(
+      `${apiHost}/v1/generation/${engineId}/text-to-image`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+          Authorization: `Bearer ${apiKey}`,
+        },
+        body: JSON.stringify({
+          text_prompts: [
+            {
+              text: prompt,
+            },
+          ],
+          cfg_scale: 7,
+          clip_guidance_preset: 'FAST_BLUE',
+          height: 512,
+          width: 512,
+          samples: 1,
+          steps: 30,
+        }),
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error(`Non-200 response: ${await response.text()}`);
+    }
+
+    interface GenerationResponse {
+      artifacts: Array<{
+        base64: string;
+        seed: number;
+        finishReason: string;
+      }>;
+    }
+
+    const responseJSON = (await response.json()) as GenerationResponse;
+
+    const image = responseJSON.artifacts[0];
+
+    const filename = resolve(pathsConfig.tmp, `${slug}.png`);
+
+    await promises.writeFile(filename, Buffer.from(image.base64, 'base64'));
+
+    return `${slug}.png`;
+  }
+}
