86e5fb4f57dabc98099bae5a791dd98455afa9e1
robertokbr <robertojuniordev@gmail.com>
2023-05-16 15:14:14 -0300

    feat: update guards to use google provider

--- src/modules/common/guards/non-blocking-jwt-auth.guard.ts
+++ src/modules/common/guards/non-blocking-jwt-auth.guard.ts
@@ -1,15 +1,90 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
-import { AuthGuard } from '@nestjs/passport';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  Logger,
+} from '@nestjs/common';
+import { UserDto } from 'src/modules/users/dto/user.dto';
+import fetch from 'node-fetch';
+import { UsersRepository } from 'src/modules/users/repositories/users.repository';
 
 @Injectable()
-export class NonBlockingJwtAuthGuard
-  extends AuthGuard(['authenticatorJwt'])
-  implements CanActivate
-{
+export class NonBlockingJwtAuthGuard implements CanActivate {
+  constructor(private usersRepository: UsersRepository) {}
+
+  private readonly logger = new Logger(NonBlockingJwtAuthGuard.name);
+  private cache = new Map<string, { user: UserDto; createdAt: number }>();
+
+  private getFromCache(jwt: string) {
+    const data = this.cache.get(jwt);
+
+    if (data) {
+      const now = Date.now();
+      const diff = now - data.createdAt;
+
+      if (diff < 1000 * 60 * 60) {
+        this.logger.debug(`returning user from cache: ${data.user}`);
+        return data.user;
+      }
+
+      this.cache.delete(jwt);
+    }
+  }
+
+  private setToCache(jwt: string, user: UserDto) {
+    this.cache.set(jwt, { user, createdAt: Date.now() });
+  }
+
+  private async getExtraUserData(user: UserDto) {
+    const extra = await this.usersRepository.findByEmail(user.email);
+
+    if (extra) {
+      return extra;
+    }
+
+    return user;
+  }
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
     try {
-      await super.canActivate(context);
-    } finally {
+      const bearerToken = context.switchToHttp().getRequest()
+        .headers.authorization;
+
+      const token = bearerToken.split(' ')[1];
+
+      this.logger.debug(`decoding token: ${token}`);
+
+      const cachedUser = this.getFromCache(token);
+
+      if (cachedUser) {
+        context.switchToHttp().getRequest().user = cachedUser;
+        return true;
+      }
+
+      const data = await fetch(
+        'https://oauth2.googleapis.com/tokeninfo?id_token=' + token,
+      );
+
+      if (data.status !== 200) {
+        const message = await data.text();
+        this.logger.error('Invalid token', message);
+
+        return false;
+      }
+
+      const json = await data.json();
+
+      const googleUser = UserDto.fromGoogleData(json);
+
+      const user = await this.getExtraUserData(googleUser);
+
+      context.switchToHttp().getRequest().user = user;
+
+      this.setToCache(token, user);
+
+      return true;
+    } catch (err) {
+      this.logger.error(err);
       return true;
     }
   }
--- src/modules/common/guards/roles.guard.ts
+++ src/modules/common/guards/roles.guard.ts
@@ -1,10 +1,16 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import {
+  Injectable,
+  CanActivate,
+  ExecutionContext,
+  Logger,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 import { Role } from '../../users/enums/role.enum';
 
 @Injectable()
 export class RolesGuard implements CanActivate {
   constructor(private reflector: Reflector) {}
+  private logger = new Logger(RolesGuard.name);
 
   canActivate(context: ExecutionContext): boolean {
     const requiredRoles = this.reflector.getAllAndOverride<Role[]>('roles', [
@@ -15,6 +21,9 @@ export class RolesGuard implements CanActivate {
       return true;
     }
     const { user } = context.switchToHttp().getRequest();
+
+    this.logger.log(user);
+
     return requiredRoles.includes(user.role);
   }
 }
