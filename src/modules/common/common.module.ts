ce003c262564a3cf32d0a10d9badd312fea0384c
robertokbr <robertojuniordev@gmail.com>
2023-05-16 15:14:27 -0300

    chore: remove strategies

--- src/modules/common/common.module.ts
+++ src/modules/common/common.module.ts
@@ -1,20 +1,10 @@
 import { Global, Module } from '@nestjs/common';
-import { JwtModule } from '@nestjs/jwt';
-import { PassportModule } from '@nestjs/passport';
 import { PrismaService } from './prisma/prisma.service';
-import { AuthenticatorJwtStrategy } from './strategies/jwt.strategy';
-import { jwtConfig } from 'src/configs/jwt.config';
 
 @Global()
 @Module({
-  imports: [
-    PassportModule,
-    JwtModule.register({
-      secret: jwtConfig.secret,
-      signOptions: { expiresIn: '7d' },
-    }),
-  ],
-  providers: [PrismaService, AuthenticatorJwtStrategy],
-  exports: [PrismaService, JwtModule],
+  imports: [],
+  providers: [PrismaService],
+  exports: [PrismaService],
 })
 export class CommonModule {}
